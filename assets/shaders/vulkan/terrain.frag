OpenStaticFish/ZigCraft

fix: PBR lighting energy conservation (#230)
Fixes two critical energy conservation violations in PBR lighting:

1. Sun emission missing π division (CRITICAL)
   - Direct lighting was 3.14x too bright because sun color
     was not divided by π while BRDF diffuse term already was
   - Added / PI division to all sun color calculations (4 locations):
     * Volumetric lighting (line 242)
     * PBR direct lighting (line 453)
     * Non-PBR blocks direct lighting (line 486)
     * LOD mode direct lighting (line 519)
   - This reduces direct lighting energy to physically correct levels

2. IBL environment map not pre-filtered
   - Environment map was sampled at fixed mip level 8.0
     regardless of surface roughness
   - Added MAX_ENV_MIPS = 8.0 constant
   - Now samples mip level based on surface roughness:
     * PBR blocks: envMipLevel = roughness * 8.0
     * Non-PBR blocks: envMipLevel = 0.5 * 8.0
   - Rough surfaces get blurrier ambient reflections
   - Smooth surfaces get sharper ambient reflections

Impact:
- Sunlit surfaces are ~3.14x less bright (physically correct)
- Ambient reflections now properly vary with roughness
- Tone-mapping handles reduced energy appropriately
- Shadows more visible in sunlit areas

Fixes #230

assets/shaders/vulkan/terrain.frag
@@ -199,6 +199,7 @@ float calculateShadow(vec3 fragPosWorld, float nDotL, int layer) {
 
 // PBR functions
 const float PI = 3.14159265359;
+const float MAX_ENV_MIPS = 8.0; // Max mip level for environment map
 
 // Henyey-Greenstein Phase Function for Mie Scattering (Phase 4)
 float henyeyGreenstein(float g, float cosTheta) {
@@ -237,8 +238,9 @@ vec4 calculateVolumetric(vec3 rayStart, vec3 rayEnd, float dither) {
     float cosTheta = dot(rayDir, normalize(global.sun_dir.xyz));
     float phase = henyeyGreenstein(global.volumetric_params.w, cosTheta);
     
-    // Use the actual sun color for scattering
-                vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0; // Significant boost
+    // Use the actual sun color for scattering (divide by PI for energy conservation if enabled)
+                float piDivVolumetric = global.pbr_params.w > 0.5 ? PI : 1.0;
+                vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0 / piDivVolumetric; // Significant boost
     vec3 accumulatedScattering = vec3(0.0);
     float transmittance = 1.0;
     // Scale density to be more manageable (0.01 in preset = light fog)
@@ -449,12 +451,15 @@ void main() {
                 vec3 kD = (vec3(1.0) - kS) * (1.0 - metallic);
                 
                 float NdotL_final = max(dot(N, L), 0.0);
-                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0;
+                float piDivPBR = global.pbr_params.w > 0.5 ? PI : 1.0;
+                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0 / piDivPBR;
                 vec3 Lo = (kD * albedo / PI + specular) * sunColor * NdotL_final * (1.0 - totalShadow);
                 
                 // Ambient lighting (IBL) - shadows reduce ambient slightly for more visible effect
+                float envRoughness = roughness;
+                float envMipLevel = envRoughness * MAX_ENV_MIPS;
                 vec2 envUV = SampleSphericalMap(normalize(N));
-                vec3 envColor = textureLod(uEnvMap, envUV, 8.0).rgb;
+                vec3 envColor = textureLod(uEnvMap, envUV, envMipLevel).rgb;
                 
                 float skyLight = vSkyLight * global.lighting.x;
                 vec3 blockLight = vBlockLight;
@@ -468,15 +473,18 @@ void main() {
                 vec3 blockLight = vBlockLight;
                 
                 // Sample IBL for ambient (even for non-PBR blocks)
+                float envRoughness = 0.5; // Default roughness for non-PBR blocks
+                float envMipLevel = envRoughness * MAX_ENV_MIPS;
                 vec2 envUV = SampleSphericalMap(normalize(N));
-                vec3 envColor = textureLod(uEnvMap, envUV, 8.0).rgb;
+                vec3 envColor = textureLod(uEnvMap, envUV, envMipLevel).rgb;
                 
                 // Shadows reduce ambient for more visible effect
                 float shadowAmbientFactor = mix(1.0, 0.2, totalShadow);
                 vec3 ambientColor = albedo * (max(min(envColor, vec3(3.0)) * skyLight * 0.8, vec3(global.lighting.x * 0.8)) + blockLight) * ao * ssao * shadowAmbientFactor;
                 
                 // Direct lighting
-                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0;
+                float piDivNonPBR = global.pbr_params.w > 0.5 ? PI : 1.0;
+                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0 / piDivNonPBR;
                 vec3 directColor = albedo * sunColor * nDotL * (1.0 - totalShadow);
                 
                 color = ambientColor + directColor;
@@ -508,7 +516,8 @@ void main() {
             float skyLightVal = vSkyLight * global.lighting.x;
             float shadowAmbientFactor = mix(1.0, 0.2, totalShadow);
             vec3 ambientColor = albedo * (max(vec3(skyLightVal * 0.8), vec3(global.lighting.x * 0.4)) + blockLight) * ao * ssao * shadowAmbientFactor;
-            vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0;
+            float piDivLOD = global.pbr_params.w > 0.5 ? PI : 1.0;
+            vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0 / piDivLOD;
             vec3 directColor = albedo * sunColor * nDotL * (1.0 - totalShadow);
             color = ambientColor + directColor;
         } else {

MichaelFisher1997/zig-opengl

assets/shaders/vulkan/terrain.frag
@@ -199,6 +199,7 @@ float calculateShadow(vec3 fragPosWorld, float nDotL, int layer) {
 
 // PBR functions
 const float PI = 3.14159265359;
+const float MAX_ENV_MIPS = 8.0; // Max mip level for environment map
 
 // Henyey-Greenstein Phase Function for Mie Scattering (Phase 4)
 float henyeyGreenstein(float g, float cosTheta) {
@@ -237,8 +238,8 @@ vec4 calculateVolumetric(vec3 rayStart, vec3 rayEnd, float dither) {
     float cosTheta = dot(rayDir, normalize(global.sun_dir.xyz));
     float phase = henyeyGreenstein(global.volumetric_params.w, cosTheta);
     
-    // Use the actual sun color for scattering
-                vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0; // Significant boost
+    // Use the actual sun color for scattering (divide by PI for energy conservation)
+                vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0 / PI; // Significant boost
     vec3 accumulatedScattering = vec3(0.0);
     float transmittance = 1.0;
     // Scale density to be more manageable (0.01 in preset = light fog)
@@ -449,12 +450,14 @@ void main() {
                 vec3 kD = (vec3(1.0) - kS) * (1.0 - metallic);
                 
                 float NdotL_final = max(dot(N, L), 0.0);
-                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0;
+                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0 / PI;
                 vec3 Lo = (kD * albedo / PI + specular) * sunColor * NdotL_final * (1.0 - totalShadow);
                 
                 // Ambient lighting (IBL) - shadows reduce ambient slightly for more visible effect
+                float envRoughness = roughness;
+                float envMipLevel = envRoughness * MAX_ENV_MIPS;
                 vec2 envUV = SampleSphericalMap(normalize(N));
-                vec3 envColor = textureLod(uEnvMap, envUV, 8.0).rgb;
+                vec3 envColor = textureLod(uEnvMap, envUV, envMipLevel).rgb;
                 
                 float skyLight = vSkyLight * global.lighting.x;
                 vec3 blockLight = vBlockLight;
@@ -468,15 +471,17 @@ void main() {
                 vec3 blockLight = vBlockLight;
                 
                 // Sample IBL for ambient (even for non-PBR blocks)
+                float envRoughness = 0.5; // Default roughness for non-PBR blocks
+                float envMipLevel = envRoughness * MAX_ENV_MIPS;
                 vec2 envUV = SampleSphericalMap(normalize(N));
-                vec3 envColor = textureLod(uEnvMap, envUV, 8.0).rgb;
+                vec3 envColor = textureLod(uEnvMap, envUV, envMipLevel).rgb;
                 
                 // Shadows reduce ambient for more visible effect
                 float shadowAmbientFactor = mix(1.0, 0.2, totalShadow);
                 vec3 ambientColor = albedo * (max(min(envColor, vec3(3.0)) * skyLight * 0.8, vec3(global.lighting.x * 0.8)) + blockLight) * ao * ssao * shadowAmbientFactor;
                 
                 // Direct lighting
-                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0;
+                vec3 sunColor = global.sun_color.rgb * global.params.w * 4.0 / PI;
                 vec3 directColor = albedo * sunColor * nDotL * (1.0 - totalShadow);
                 
                 color = ambientColor + directColor;
@@ -508,7 +513,7 @@ void main() {
             float skyLightVal = vSkyLight * global.lighting.x;
             float shadowAmbientFactor = mix(1.0, 0.2, totalShadow);
             vec3 ambientColor = albedo * (max(vec3(skyLightVal * 0.8), vec3(global.lighting.x * 0.4)) + blockLight) * ao * ssao * shadowAmbientFactor;
-            vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0;
+            vec3 sunColor = global.sun_color.rgb * global.params.w * 3.0 / PI;
             vec3 directColor = albedo * sunColor * nDotL * (1.0 - totalShadow);
             color = ambientColor + directColor;
         } else {